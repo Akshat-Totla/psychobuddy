3eb8c7a15e2dbecc02b2a734f83b6ae8dbea80be
Akshat-Totla <140895568+Akshat-Totla@users.noreply.github.com>
2025-04-27 17:05:00 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 // models/User.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -14,8 +14,8 @@ const UserSchema = new mongoose.Schema({
 // Hash password before saving
 UserSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
     next();
 });
 
